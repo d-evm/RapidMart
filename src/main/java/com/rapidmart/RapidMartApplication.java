d142ab69cede5b855dcc59d9bf649c18dcd0d40d
d-evm <devamsonigra@gmail.com>
2025-06-09 00:21:23 +0530

    Initial commit

---
+++ src/main/java/com/rapidmart/RapidMartApplication.java
@@ -0,0 +1,13 @@
+package com.rapidmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidMartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rapidmart/RapidMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidMartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
